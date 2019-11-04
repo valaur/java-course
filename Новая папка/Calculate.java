9af9c505d2959066276071b9286bfaab23cfa1d4
valaur <valera.berserk@gmail.com>
2019-11-04 17:21:27 +0500

    usser input

--- Новая папка/Calculate.java
+++ Новая папка/Calculate.java
@@ -4,10 +4,6 @@ public class Calculate {
 		int one = Integer.valueOf(args[0]);
 		int two = Integer.valueOf(args[1]);
 		int sum = one+two;
-		int mlt = one*two;
-		double div = one/two;
-		System.out.println("sum is "+ sum + " mlt is "+mlt);
-		System.out.println("div is "+ (double)one/two);
-		
+		System.out.println(sum);
 	}
 }
